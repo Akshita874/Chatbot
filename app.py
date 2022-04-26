9bcc13f815944d9a62065c24232f68d4ea4b3aa6
Akshita874 <104422451+Akshita874@users.noreply.github.com>
2022-04-26 14:11:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from requests import get
+from bs4 import BeautifulSoup
+import os
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+bot= ChatBot('ChatBot')
+
+trainer = ListTrainer(bot)
+
+for file in os.listdir('data/'):
+
+    chats = open('data/' + file, 'r').readlines()
+
+    trainer.train(chats)
+
+@app.route("/")
+def hello():
+    return render_template('chat.html')
+
+@app.route("/ask", methods=['POST'])
+def ask():
+
+    message = str(request.form['messageText'])
+    print(message)
+    bot_response = bot.get_response(message)
+    print(bot_response)
+
+    while True:
+
+        if bot_response.confidence > 0.1:
+
+            bot_response = str(bot_response)      
+            print(bot_response)
+            return jsonify({'status':'OK','answer':bot_response})
+ 
+        elif message == ("bye"):
+
+            bot_response='Hope to see you soon'
+
+            print(bot_response)
+            return jsonify({'status':'OK','answer':bot_response})
+      
+        else:
+        
+            try:
+                url  = "https://en.wikipedia.org/wiki/"+ message
+                page = get(url).text
+                soup = BeautifulSoup(page,"html.parser")
+                p    = soup.find_all("p")
+                return jsonify({'status':'OK','answer':p[1].text})
+
+            except IndexError as error:
+
+                bot_response = 'Sorry i have no idea about that.'
+            
+                print(bot_response)
+                return jsonify({'status':'OK','answer':bot_response})
+
